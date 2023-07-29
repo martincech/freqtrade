f48ab38717fc846e52ddb21bbf97298fe085f169
Martin Čech <cech.mit@gmail.com>
2023-07-29 14:44:08 +0200

    xtb exchange future trading

--- freqtrade/exchange/xtb.py
+++ freqtrade/exchange/xtb.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple, Dict
 
-from freqtrade.enums import MarginMode, TradingMode
+from freqtrade.enums import MarginMode, TradingMode, PriceType
 from freqtrade.exchange import Exchange
 
 
@@ -14,4 +14,22 @@ class Xtb(Exchange):
         "ohlcv_candle_limit": 100,
         "ohlcv_partial_candle": False,
         "ohlcv_require_since": True,
+        "ohlcv_has_history": True,
+        "ccxt_futures_name": "future",
+    }
+
+    _ft_has_futures: Dict = {
+        "mark_ohlcv_timeframe": "4h",
+        "funding_fee_timeframe": "8h",
+        "stoploss_on_exchange": True,
+        "stoploss_order_types": {
+            "limit": "limit",
+            "market": "market"
+        },
+        "stop_price_type_field": "triggerBy",
+        "stop_price_type_value_mapping": {
+            PriceType.LAST: "LastPrice",
+            PriceType.MARK: "MarkPrice",
+            PriceType.INDEX: "IndexPrice",
+        },
     }
